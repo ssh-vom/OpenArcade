5491dc6d01259ea3e36a3e2c0e2f4fcf40bb650c
Shivom Sharma <shars119@mcmaster.ca>
2025-11-07 19:59:29 -0500

    fixing sample aggregation

--- server/aggregator.py
+++ server/aggregator.py
@@ -4,32 +4,32 @@ from multiprocessing import Queue, Process
 
 
 # HID Thread
-def hid(q):
-    os.sched_setaffinity(0, {0})
+def hid(q, core: int):
+    os.sched_setaffinity(0, {core})
     while True:
         msg = q.get()
-        print(f"[HID] Got message: {msg}")
+        print(f"[HID] Got message: {msg} from core: {core}")
 
 
 # Pairing Thread
 
 
-def pair(q):
-    os.sched_setaffinity(0, {1})
+def pair(q, core: int):
+    os.sched_setaffinity(0, {core})
 
 
 # Polling Thread
-def poll(q):
-    os.sched_setaffinity(0, {2})
+def poll(q, core: int):
+    os.sched_setaffinity(0, {core})
     while True:
         for i in range(10):
-            q.put(f"msg[{i}]")
+            q.put(f"msg[{i}] sent from core: {core}")
 
 
 if __name__ == "__main__":
     mp.set_start_method("fork")
     q = Queue()
-    p1 = Process(target=hid, args=(q,))
-    p2 = Process(target=hid, args=(q,))
+    p1 = Process(target=hid, args=(q, 1))
+    p2 = Process(target=poll, args=(q, 2))
     p1.start()
     p2.start()
