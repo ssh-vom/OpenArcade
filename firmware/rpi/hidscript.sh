8838274a4a6e1c0fc737595c02733060d90f741a
Shivom Sharma <shars119@mcmaster.ca>
2026-04-05 00:44:17 -0400

    update the usage page descriptor

--- firmware/rpi/hidscript.sh
+++ firmware/rpi/hidscript.sh
@@ -116,43 +116,43 @@ echo 8 > functions/hid.usb1/report_length
 # Byte 3-6: Axes (Left X, Left Y, Right X, Right Y - centered at 0x80)
 # Byte 7: Reserved
 echo -ne \
-'\x05\x01'\          # Usage Page (Generic Desktop)
-'\x09\x05'\          # Usage (Game Pad)
-'\xa1\x01'\          # Collection (Application)
-'\x05\x09'\          #   Usage Page (Button)
-'\x19\x01'\          #   Usage Minimum (Button 1)
-'\x29\x10'\          #   Usage Maximum (Button 16)
-'\x15\x00'\          #   Logical Minimum (0)
-'\x25\x01'\          #   Logical Maximum (1)
-'\x75\x01'\          #   Report Size (1)
-'\x95\x10'\          #   Report Count (16)
-'\x81\x02'\          #   Input (Data,Var,Abs)
-'\x05\x01'\          #   Usage Page (Generic Desktop)
-'\x09\x39'\          #   Usage (Hat switch)
-'\x15\x00'\          #   Logical Minimum (0)
-'\x25\x07'\          #   Logical Maximum (7)
-'\x35\x00'\          #   Physical Minimum (0)
-'\x46\x3b\x01'\      #   Physical Maximum (315)
-'\x65\x14'\          #   Unit (Degrees)
-'\x75\x04'\          #   Report Size (4)
-'\x95\x01'\          #   Report Count (1)
-'\x81\x42'\          #   Input (Data,Var,Abs,Null)
-'\x75\x04'\          #   Report Size (4)
-'\x95\x01'\          #   Report Count (1)
-'\x81\x01'\          #   Input (Const) - padding
-'\x09\x30'\          #   Usage (X)
-'\x09\x31'\          #   Usage (Y)
-'\x09\x32'\          #   Usage (Z)
-'\x09\x35'\          #   Usage (Rz)
-'\x15\x00'\          #   Logical Minimum (0)
-'\x26\xff\x00'\      #   Logical Maximum (255)
-'\x75\x08'\          #   Report Size (8)
-'\x95\x04'\          #   Report Count (4)
-'\x81\x02'\          #   Input (Data,Var,Abs)
-'\x75\x08'\          #   Report Size (8)
-'\x95\x01'\          #   Report Count (1)
-'\x81\x01'\          #   Input (Const) - reserved byte
-'\xc0'\              # End Collection
+'\x05\x01'\
+'\x09\x05'\
+'\xa1\x01'\
+'\x05\x09'\
+'\x19\x01'\
+'\x29\x10'\
+'\x15\x00'\
+'\x25\x01'\
+'\x75\x01'\
+'\x95\x10'\
+'\x81\x02'\
+'\x05\x01'\
+'\x09\x39'\
+'\x15\x00'\
+'\x25\x07'\
+'\x35\x00'\
+'\x46\x3b\x01'\
+'\x65\x14'\
+'\x75\x04'\
+'\x95\x01'\
+'\x81\x42'\
+'\x75\x04'\
+'\x95\x01'\
+'\x81\x01'\
+'\x09\x30'\
+'\x09\x31'\
+'\x09\x32'\
+'\x09\x35'\
+'\x15\x00'\
+'\x26\xff\x00'\
+'\x75\x08'\
+'\x95\x04'\
+'\x81\x02'\
+'\x75\x08'\
+'\x95\x01'\
+'\x81\x01'\
+'\xc0' \
 > functions/hid.usb1/report_desc
 
 ln -s functions/hid.usb1 configs/c.1/
